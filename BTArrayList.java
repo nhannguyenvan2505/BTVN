587e6898420723469e564b7c3131132f8cb2834c
nhannguyenvan2505 <nhann0515@gmail.com>
2024-09-04 08:21:02 +0700

    BTVN 4thg9

--- BTArrayList.java
+++ BTArrayList.java
@@ -45,15 +45,59 @@ public class BTArrayList {
         arrayList.removeIf(number -> number % 2 != 0);
     }
 
+    public static void displayMenu() {
+        ArrayList<Integer> arrayList = null;
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("\n--- MENU ---");
+            System.out.println("1. Nhap mang");
+            System.out.println("2. Xuat mang");
+            System.out.println("3. Tim phan tu lon thu 2");
+            System.out.println("4. Xoa so le ra khoi mang");
+            System.out.println("0. Thoat");
+            System.out.print("Chon chuc nang: ");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    arrayList = inputArrayList();
+                    break;
+                case 2:
+                    if (arrayList != null) {
+                        printArrayList(arrayList);
+                    } else {
+                        System.out.println("Ban chua nhap mang.");
+                    }
+                    break;
+                case 3:
+                    if (arrayList != null) {
+                        int secondLargest = findSecondLargest(arrayList);
+                        if (secondLargest != Integer.MIN_VALUE) {
+                            System.out.println("Phan tu lon thu 2 trong mang la: " + secondLargest);
+                        }
+                    } else {
+                        System.out.println("Ban chua nhap mang.");
+                    }
+                    break;
+                case 4:
+                    if (arrayList != null) {
+                        removeOddNumbers(arrayList);
+                        System.out.println("Mang sau khi xoa le: ");
+                        printArrayList(arrayList);
+                    } else {
+                        System.out.println("Ban chua nhap mang.");
+                    }
+                    break;
+                case 0:
+                    System.out.println("Exit.");
+                    break;
+                default:
+                    System.out.println("Lua chon khong hop le. Vui long chon lai.");
+            }
+        } while (choice != 0);
+    }
+
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList = inputArrayList();
-        printArrayList(arrayList);
-        int max2 = findSecondLargest(arrayList);
-        if (max2 != Integer.MIN_VALUE) {
-            System.out.println("Phan tu lon thu 2 trong mang la: " + max2);
-        }
-        removeOddNumbers(arrayList);
-        System.out.println("Mang sau khi xoa le: ");
-        printArrayList(arrayList);
+        displayMenu();
     }
 }
